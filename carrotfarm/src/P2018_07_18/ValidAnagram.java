da2605c8ae5a75e8e48ff591682319b0dfdbf793
carrotfarm <carrot.carrotfarm@gmail.com>
2018-07-21 15:38:40 +0900

    Anagram

---
+++ carrotfarm/src/P2018_07_18/ValidAnagram.java
@@ -0,0 +1,37 @@
+package P2018_07_18;
+
+/**
+ * Created on 2018. 7. 21..
+ */
+public class ValidAnagram {
+
+	public static void main(String[] args) {
+
+//		String s = "anagram";
+//		String t = "nagaram";
+
+		String s = "rat";
+		String t = "car";
+		boolean result = new ValidAnagram().isAnagram(s, t);
+		System.out.println(result);
+	}
+
+	public boolean isAnagram(String s, String t) {
+		boolean result = true;
+		int[] data = new int[26];
+
+		for (int i=0;i<s.length();i++) {
+			data[s.charAt(i)-'a']++;
+		}
+		for (int i=0;i<t.length();i++) {
+			data[t.charAt(i)-'a']--;
+		}
+		for (int i=0;i<data.length;i++) {
+			if (data[i]!=0) {
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
+}
---
+++ carrotfarm/src/P2018_07_18/ValidAnagram2.java
@@ -0,0 +1,16 @@
+package P2018_07_18;
+
+/**
+ * Created on 2018. 7. 21..
+ */
+public class ValidAnagram2 {
+
+
+
+	class Solution {
+		public boolean isAnagram(String s, String t) {
+
+			return false;
+		}
+	}
+}
