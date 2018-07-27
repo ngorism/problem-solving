45af82bea782886700ff1fd106d9ca4316f8a6d2
sanghee <afk.sh94@gmail.com>
2018-07-28 00:24:20 +0900

    20. Valid Parentheses

--- shpark/ValidParentheses.java
+++ shpark/ValidParentheses.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertTrue;
 
 public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='{')stack.push('}');
+            else if (c=='[') stack.push(']');
+            else if (c=='(') stack.push(')');
+            else if(stack.isEmpty() || c!=stack.pop())return false;
+        }
+        return stack.isEmpty();
+    }
+
+    @Test
+    public void test() {
+        assertTrue(isValid("{[]}"));
+    }
 }
