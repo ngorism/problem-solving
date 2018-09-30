bbefb1c00bed130d1c64eace815c9af3f2468d71
jeongmin <funkytoy@naver.com>
2018-09-30 22:19:38 +0900

    Middle of the Linked List(Easy)

---
+++ jmlee/2018-09-18/Middle of the Linked List(Easy)/main.kt
@@ -0,0 +1,38 @@
+fun main(args: Array<String>) {
+
+
+}
+
+class ListNode(var `val`: Int = 0) {
+    var next: ListNode? = null
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int = 0) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun middleNode(head: ListNode?): ListNode? {
+        head ?: return null
+
+        var cur: ListNode? = head
+        var mid: ListNode? = null
+        var index = 1
+        var midIndex = 0
+
+        while (cur != null) {
+            val tmp = index / 2 + 1
+            if (tmp != midIndex) {
+                midIndex = tmp
+                mid = if (mid == null) head else mid.next
+            }
+
+            index++
+            cur = cur.next
+        }
+
+        return mid
+    }
+}
